fd0e93595c4b0d30c564f84d18b31e6cc125a08a
RAJENDRA <rajendrajfc1021@gmail.com>
2023-04-19 16:41:04 +0530

    friend function

---
+++ tut25.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class Complex
+{
+    int a,b;
+
+public:
+    void SetNUmber(int n1, int n2){
+        a = n1;
+        b = n2;
+    }
+    void PrintNumber(){
+        cout<<"Your Number is "<< a << " + " << b << "i" << endl;
+    }
+
+};
+
+int calculator :: super
+
+int main(){
+    
+    return 0;
+}
