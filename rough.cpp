f7f6ee5c261ddb8914f2898500f9827ea7981bb9
RAJENDRA <rajendrajfc1021@gmail.com>
2023-04-19 16:45:18 +0530

    friend function

--- rough.cpp
+++ rough.cpp
@@ -1,35 +1,29 @@
-#include<iostream>
-using namespace std;
+class Complex
+{
+    int a, b;
+    // Individually declaring functions as friends
+     friend int Calculator ::sumRealComplex(Complex, Complex);
+     friend int Calculator ::sumCompComplex(Complex, Complex);
 
-class complex{
-    int a;
-    int b;
+public:
+    void setNumber(int n1, int n2)
+    {
+        a = n1;
+        b = n2;a
+    }
 
-    public: 
-        void setData(int v1, int v2){
-            a = v1;
-            b = v2;
-        }
-
-        void setDataBySum(complex o1, complex o2){
-            a = o1.a + o2.a;
-            b = o1.b + o2.b;
-        }
-
-        void printNumber(){
-            cout<<"Your complex number is "<<a<<" + "<<b<<"i"<<endl;
-        }
+    void printNumber()
+    {
+        cout << "Your number is " << a << " + " << b << "i" << endl;
+    }
 };
 
-int main(){
-    complex c1, c2, c3;
-    c1.setData(1, 2);
-    c1.printNumber();
+int Calculator ::sumRealComplex(Complex o1, Complex o2)
+{
+    return (o1.a + o2.a);
+}
 
-    c2.setData(3, 4);
-    c2.printNumber();
-
-    c3.setDataBySum(c1, c2);
-    c3.printNumber();
-    return 0;
+int Calculator ::sumCompComplex(Complex o1, Complex o2)
+{
+    return (o1.b + o2.b);
 }
